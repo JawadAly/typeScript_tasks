08f40d44a43d45d16f3db8297ff67177c8f48119
jawadSaleem <jawadsaleem932@gmail.com>
2024-02-23 21:09:23 +0500

    added question 5

---
+++ question5.js
@@ -0,0 +1,6 @@
+var personName = "\tJawad\t\n";
+console.log("The person's name printed with white spaces is as follows:");
+console.log(personName);
+console.log("The person's name printed without white spaces is as follows:");
+personName = personName.trim();
+console.log(personName);
---
+++ question5.ts
@@ -0,0 +1,5 @@
+let personName = "\tJawad\t\n";
+console.log("The person's name printed with white spaces is as follows:");
+console.log(personName);
+console.log("The person's name printed without white spaces is as follows:");
+console.log(personName.trim());
